984a3a404f36113a55c7940044edf218a54b6527
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-10-12 07:47:55 -0600

    testing areas

--- src/TestAll.java
+++ src/TestAll.java
@@ -1,12 +1,47 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestAll {
+
+	@Before
+	public void initialize() {
+
+	}
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	// BOARD
+
+	// TOKEN
+
+	// PLAYER
+
+	// DICE
+
+	// BOARD SPACE
+
+	// BANK
+
+	// TAX
+
+	// OPEN SPACE
+
+	// PROPERTY
+
+	// GO TO JAIL
+
+	// JAIL
+
+	// UTILITY
+
+	// RAILROAD
+
+	// STREET
+
+	// HOUSE/HOTEL
 
 }

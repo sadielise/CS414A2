ceb769718063b9c17d9c4fe3ce5845d917b6e7b3
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-10-19 15:46:15 -0600

    methods, attributes, and tests

--- src/a4/domain/Board.java
+++ src/a4/domain/Board.java
@@ -1,5 +1,19 @@
 package a4.domain;
 
-public class Board {
+import java.util.List;
 
+public class Board {
+	List<BoardSpace> spaces;
+
+	public Board(List<BoardSpace> board_spaces) {
+		spaces = board_spaces;
+	}
+
+	public void addSpace(BoardSpace space_to_add) {
+		spaces.add(space_to_add);
+	}
+
+	public void removeSpace(BoardSpace space_to_remove) {
+		spaces.remove(space_to_remove);
+	}
 }
--- src/a4/domain/BoardSpace.java
+++ src/a4/domain/BoardSpace.java
@@ -1,5 +1,32 @@
 package a4.domain;
 
-public class BoardSpace {
+import java.util.ArrayList;
 
+public class BoardSpace {
+	int location;
+	ArrayList<Player> players = new ArrayList<Player>();
+
+	public BoardSpace(int location) {
+		this.location = location;
+	}
+
+	public int getLocation() {
+		return location;
+	}
+
+	public void setLocation(int new_location) {
+		location = new_location;
+	}
+
+	public void addPlayer(Player player_to_add) {
+		players.add(player_to_add);
+	}
+
+	public boolean removePlayer(Player player_to_remove) {
+		return players.remove(player_to_remove);
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
 }

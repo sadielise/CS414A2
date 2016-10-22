9968daa89b002430c3854e18634aa90fb826ecdc
dbthorpe <dbthorpe@gmail.com>
2016-10-22 13:20:20 -0600

    added Controller.java Model.java and View.java to the files tracked by git (I thought I did this on the first commit)

--- src/a4/gui/Model.java
+++ src/a4/gui/Model.java
@@ -1,5 +1,17 @@
 package a4.gui;
 
 public class Model {
-
+	private View view;
+	
+	public void setView(View v){
+		view = v;
+	}
+	
+	public String getCurrentPlayer(){
+		return "non";
+	}
+	
+	public String getCurrentBankroll(){
+		return "non";
+	}
 }

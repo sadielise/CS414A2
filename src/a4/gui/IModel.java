aef5acfbdaba58863c00de62bf377ed1d712d824
David Thorpe <dbthorpe@gmail.com>
2016-10-25 15:27:57 -0600

    Implemented gui components for methods available in IModel. Still need to do the Auction Dialog and anything else that is added to IModel.

---
+++ src/a4/gui/IModel.java
@@ -0,0 +1,9 @@
+package a4.gui;
+
+public interface IModel {
+	public void propertyIsUnowned(String propertyName, int propertyValue);
+	public void unableToPayRentTo(String playerName , int rentAmount);
+	public void paidRentTo(String playerName , int rentAmount);
+	public void playerSentToJail(String playerName);
+	public void propertyCannotBeDeveloped(String propertyName);
+}
--- src/a4/gui/Model.java
+++ src/a4/gui/Model.java
@@ -7,82 +7,116 @@ import javax.swing.ImageIcon;
 
 import a4.domain.IMonopolyGame;
 
-public class Model {
-	
+public class Model implements IModel {
+
 	private View view;
 	private IMonopolyGame game;
 	boolean isStarted = false;
-	
+	boolean hasRolled = false;
+
 	public void setView(View v){
 		view = v;
 	}
-	
+
 	public void setGame(IMonopolyGame g){
 		game = g;
 	}
-	
+
 	public String getPlayer(){
 		return game.getCurrentPlayer();
 	}
-	
+
 	public int getLocation(String player){
 		return game.getLocation(player);
 	}
-	
+
 	public List<String> getPlayers(){
 		return game.getPlayers();
 	}
-	
+
 	public String getCurrentBankroll(){
 		return ""+game.getBankroll(game.getCurrentPlayer());
 	}
-	
+
 	public List<String> getCurrentPlayersProperties(){
 		return game.getProperties(game.getCurrentPlayer());
 	}
-	
+
 	public List<String> getPlayersProperties(String player){
 		return game.getProperties(game.getCurrentPlayer());
 	}
-	
+
+	public void purchaseAuctionedProperty(List<Double> offers){
+		game.purchaseAuctionedProperty(offers);
+	}
 	public String getProperty(int location){
 		return game.getProperty(location);
 	}
-	
+
 	public void develop(String property){
 		game.developProperty(property);
 	}
-	
+
 	public void roll(){
-		game.roll();
+		if(!hasRolled){
+			game.roll();
+			hasRolled = true;
+		}
 	}
-	
+
+	public void purchaseProperty(String player, String property){
+		game.purchaseProperty(player,property);
+	}
 	public void trade(String currProperty, String otherProperty){
 		game.trade(currProperty, otherProperty);
 	}
-	
+
 	public void startNewGame(List<String> playerNames, int timeInMinutes){
 		game.newGame(playerNames,timeInMinutes);
 		isStarted = true;
 	}
-	
+
 	public void endTurn(){
 		game.endTurn();
-		
+
 	}
-	
+
 	public Icon getIcon(String player){
 		int playerNumber = game.getPlayerNumber(player);
 		Icon currentIcon = new ImageIcon("filepath/icon"+playerNumber);
 		return null;
 	}
-	
+
 	public void undevelop(String property){
 		game.undevelop(property);
+		update();
 	}
-	
+
+	public void propertyIsUnowned(String propertyName, int propertyValue) {
+		update();
+		view.unownedPropertyDialog(propertyName, propertyValue);
+	}
+
+	public void unableToPayRentTo(String playerName, int rentAmount) {
+		update();
+		view.unableToPayDialog(playerName, rentAmount);
+	}
+
+	public void paidRentTo(String playerName, int rentAmount) {
+		update();
+		view.paidRentDialog(playerName, rentAmount);
+	}
+
+	public void playerSentToJail(String playerName) {
+		update();
+		view.sentToJailDialog(playerName);
+	}
+
+	public void propertyCannotBeDeveloped(String propertyName) {
+		view.propertyCannotBeDevelopedDialog(propertyName);
+	}
+
 	private void update(){
 		view.update();
 	}
-	
 }

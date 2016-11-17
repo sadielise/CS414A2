3d8bd272459a45adc8b62f7ce5bc12aa11b4c9e2
Sadie <sadiet@rams.colostate.edu>
2016-11-16 19:51:59 -0700

    still working on stuff

--- src/a4/test/BoardSpaceTest.java
+++ src/a4/test/BoardSpaceTest.java
@@ -1,18 +1,15 @@
 package a4.test;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import a4.domain.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class BoardSpaceTest {
-	BoardSpaceFactory test_factory;
-	OpenSpace test_space;
-	ArrayList<Player> test_players = new ArrayList<Player>();
+	BoardSpaceFactory testFactory;
+	OpenSpace testSpace;
+	ArrayList<Player> testPlayers = new ArrayList<Player>();
 	Player player1 = new Player("Test Player 1", 1500, 0);
 	Player player2 = new Player("Test Player 2", 1500, 0);
 	Player player3 = new Player("Test Player 3", 1500, 0);
@@ -20,23 +17,23 @@ public class BoardSpaceTest {
 
 	@Before
 	public void initialize() {
-		test_space =(OpenSpace) BoardSpaceFactory.getBoardSpace(BoardSpaceType.OPEN);
+		testSpace =(OpenSpace) BoardSpaceFactory.getBoardSpace(BoardSpaceType.OPEN);
 	}
 
 	// Construct
 	@Test
 	public void testBoardSpaceConstructor() {
-		assertEquals(OpenSpace.class, test_space.getClass());
+		assertEquals(OpenSpace.class, testSpace.getClass());
 	}
 
 	@Test
 	public void testGetLocation() {
-		assertEquals(3, test_space.getLocation());
+		assertEquals(3, testSpace.getLocation());
 	}
 
 	@Test
 	public void testSetLocation() {
-		test_space.setLocation(17);
-		assertEquals(17, test_space.getLocation());
+		testSpace.setLocation(17);
+		assertEquals(17, testSpace.getLocation());
 	}
 }

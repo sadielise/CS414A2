a7351501f57923a88d0b9c30687bafb148acd47d
Sadie <sadiet@rams.colostate.edu>
2016-11-16 19:53:59 -0700

    fixing test case issues

--- src/a4/test/BoardSpaceTest.java
+++ src/a4/test/BoardSpaceTest.java
@@ -1,7 +1,6 @@
 package a4.test;
 
 import static org.junit.Assert.*;
-import java.util.ArrayList;
 import a4.domain.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +8,6 @@ import org.junit.Test;
 public class BoardSpaceTest {
 	BoardSpaceFactory testFactory;
 	OpenSpace testSpace;
-	ArrayList<Player> testPlayers = new ArrayList<Player>();
-	Player player1 = new Player("Test Player 1", 1500, 0);
-	Player player2 = new Player("Test Player 2", 1500, 0);
-	Player player3 = new Player("Test Player 3", 1500, 0);
-	Player player4 = new Player("Test Player 4", 1500, 0);
 
 	@Before
 	public void initialize() {
@@ -24,11 +18,6 @@ public class BoardSpaceTest {
 	@Test
 	public void testBoardSpaceConstructor() {
 		assertEquals(OpenSpace.class, testSpace.getClass());
-	}
-
-	@Test
-	public void testGetLocation() {
-		assertEquals(3, testSpace.getLocation());
 	}
 
 	@Test
